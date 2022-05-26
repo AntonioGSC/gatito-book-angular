52a165acfaf6002de6001bc0ac35082c81d3a6f9
tony <antoniogabrielagsc@gmail.com>
2022-05-26 16:27:57 -0300

    Template Forms and Reactive Forms

---
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: Usuario){
+      return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+}
